112fe4a1d7e993df9ab0d7f7e6bdd2e912037f25
r4ph92 <raphael.houle03@gmail.com>
2026-04-08 15:59:12 -0400

    initial commit

---
+++ src/main/java/com/magoo/magoo/MagooApplication.java
@@ -0,0 +1,13 @@
+package com.magoo.magoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagooApplication.class, args);
+    }
+
+}
