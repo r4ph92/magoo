96a7e0923706d47e5e22ce2f698073143ca9bdd6
r4ph92 <raphael.houle03@gmail.com>
2026-04-08 16:09:01 -0400

    Add files via upload
    initial commit

---
+++ magoo/magoo/src/test/java/com/magoo/magoo/MagooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magoo.magoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
